92bb5afda259313a2c3a86b44d88b0ba9cc1e28f
ang99le <malak.d993@gmail.com>
2024-04-22 04:37:38 +0300

    responsivecheck

--- index.js
+++ index.js
@@ -29,10 +29,28 @@ function myFunction() {
 }
 //downloading cv
 document.querySelector('button').onclick = function () {
+// assigned a href attribute, which is set to the URL of the file to be downloaded
     var link = document.createElement('a');
-    link.href = 'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf';
+    link.href = "https://drive.google.com/file/d/1iNV2SE7likoUpntGo-qEwCrVaIIk-KBR/view?usp=sharing";
+// download the URL instead of navigating to it and also provides a default filename for the downloaded file.
     link.download = 'file.pdf';
+//This makes the anchor element part of the dom and it's not visible for the user
     document.body.appendChild(link); 
     link.click();
+//clean up the DOM and remove the anchor element that was temporarily added
     document.body.removeChild(link);
+}
+
+
+window.onload = function() {
+  document.getElementById('ContactMe').addEventListener('submit', function(event) {
+      event.preventDefault();
+      // these IDs from the previous steps
+      emailjs.sendForm('contact_service', 'ContactMe', this)
+          .then(() => {
+              console.log('SUCCESS!');
+          }, (error) => {
+              console.log('FAILED...', error);
+          });
+  });
 }
